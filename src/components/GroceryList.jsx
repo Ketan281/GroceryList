ca28d7ba1328f80cdd75677617c5574081fa2f6c
ketan <ketanmohite8307@gmail.com>
2022-03-19 18:04:29 +0530

    grocery assignment"

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GroceryList = ({title,status,id,handleDelete}) => {
     return (<div>
 <h1>{title}</h1>
-<button onClick={handleDelete(id)}>Delete</button>
+<button onClick={()=>handleDelete(id)}>Delete</button>
     </div>)
 }
 export {GroceryList};
